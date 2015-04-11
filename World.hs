82bbd79a975763e24ea176c52772bdf04158b6d5
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2015-04-11 16:28:37 +0200

    Finished the intensity map stuff. Breeze now works.

--- World.hs
+++ World.hs
@@ -1,5 +1,7 @@
 module World where
 
+import Control.Monad ((>=>))
+import Data.List (foldl')
 import qualified Data.Map as M
 import Data.Ratio
 import Math.Geometry.Grid
@@ -7,6 +9,8 @@ import Math.Geometry.Grid.Square
 
 import Types
 import Agent
+
+type IntensityMap = M.Map CellInd Rational
 
 cellData :: World s -> CellInd -> Maybe (CellData s)
 cellData = flip M.lookup . wCellData
@@ -19,7 +23,7 @@ edgeData = flip M.lookup . wEdgeData
 makeWorld :: [(CellInd, CellData s)]
           -> [(EdgeInd, EdgeData)]
           -> World s
-makeWorld cells edges = initEnv newWorld
+makeWorld cells edges = initBreeze newWorld
    where
       newWorld = World (WD 25 Temperate)
                        UnboundedSquareGrid
@@ -49,30 +53,52 @@ advanceGlobalData (WD time _) =
    in
       WD time temp'
 
--- |Initialize the breeze around the pits
-initEnv :: World s -> World s
-initEnv = undefined
-
--- |Sets the intensity of a sensation (stench, breeze) around a stimulus
--- (wumpus, pit).
-setIntensity :: World s
-             -> CellInd
-             -> (Rational -> CellData s) -- ^The setter for the sensation.
-             -> World s
-setIntensity world i updF = undefined
+-- |Initialize the breeze around the pits.
+initBreeze:: World s -> World s
+initBreeze world = applyIntensityMap setBreeze (intensityMap $ pits world) world
    where
+      setBreeze b c = c{breeze = b}
+      pits = map fst . M.toList . M.filter pit . wCellData
+
+-- |Applies an intensity map to a world, overwriting the values in affected cells.
+applyIntensityMap :: (Rational -> CellData s -> CellData s) -- ^Setter for a cell.
+                  -> IntensityMap
+                  -> World s
+                  -> World s
+applyIntensityMap set intM world = world{wCellData = cellData'}
+   where
+      cellData' = M.intersectionWith set intM $ wCellData world
+
+-- |Creates a map with sensation intenstities.
+--  The given list of @CellInd@ are the sources from which sensations (breeze,
+--  stench) emanate. If intensities overlap, the maximum value is taken.
+intensityMap :: [CellInd]
+             -> IntensityMap
+intensityMap = foldl' (M.unionWith max) M.empty . map getIntensity
+
+-- |Gets the intensity of a sensation (stench, breeze) around a given cell.
+getIntensity :: CellInd -> IntensityMap
+getIntensity v = foldl' addCell M.empty $ neighbourhood v
+   where
+      addCell m w = M.insert w (intensity v w) m
+
+      neighbours' :: CellInd -> [CellInd]
+      neighbours' = neighbours UnboundedSquareGrid
+      neighbourhood = neighbours' >=> neighbours' >=> neighbours'
+
       intensity :: CellInd -> CellInd -> Rational
-      intensity src j = pos $ (1 - (pos $ dist src j - 1)) / 3
+      intensity v w = pos $ (1 - (pos $ dist v w - 1)) / 3
+
 
 -- |Uniformly reduces the intensity of a sensation (stench) in the whole world
 --  by 1/3, to a minimum of 0.
-redudceIntensity :: World s
-                 -> (CellData s -> Rational) -- ^Getter.
-                 -> (Rational -> CellData s) -- ^Setter.
-                 -> World s
-redudceIntensity w getF updF = w{wCellData=cellData'}
+reduceIntensity :: World s
+                -> (CellData s -> Rational) -- ^Getter for the sensation.
+                -> (Rational -> CellData s) -- ^Setter for the sensation.
+                -> World s
+reduceIntensity world getF updF = world{wCellData=cellData'}
    where
-      cellData' = fmap (updF . pos . subtract (1 % 3) . getF) (wCellData w)
+      cellData' = fmap (updF . pos . subtract (1 % 3) . getF) (wCellData world)
 
 -- |Gets the Euclidean distance between two cells.
 dist :: CellInd -> CellInd -> Rational
@@ -83,7 +109,7 @@ dist (x,y) (x',y') = round $ sqrt $ fromIntegral $ (xd ^ 2) + (yd ^ 2)
       yd = abs $ y - y'
 
 -- |Moves the Wumpuses on a given cell and updates the stench.
-moveWumpuses :: World s -> CellInd -> World s
+moveWumpuses :: World s-> World s
 moveWumpuses = undefined
 
 -- |Regenerates the plants on a given cell.
