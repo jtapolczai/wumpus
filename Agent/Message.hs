e941a47a1e3a79d5012e804d58858c8cd3a73fae
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2015-04-29 13:47:47 +0200

    Added Ord-instance for Message.

--- Agent/Message.hs
+++ Agent/Message.hs
@@ -19,3 +19,18 @@ data Message =
    | Control
    | Action
    | GestureM EntityName GestureName
+
+-- |A simple, per-constructor comparison of messages.
+val :: Message -> Int
+val Affective{} = 0
+val VisualPerception{} = 1
+val LocalPerception{} = 2
+val GlobalPerception{} = 3
+val PositionPerception{} = 4
+val ControlPerception{} = 5
+val Control{} = 6
+val Action{} = 7
+val GestureM{} = 8
+
+instance Ord Message where
+   compare a b = compare (val a) (val b)
