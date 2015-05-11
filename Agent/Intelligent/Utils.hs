a891b6f62f9df203748967a0677467d53833aabc
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2015-05-11 15:54:36 +0200

    Added firstWhere to Agent.Intelligent.Utils.

--- Agent/Intelligent/Utils.hs
+++ Agent/Intelligent/Utils.hs
@@ -2,10 +2,14 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Agent.Intelligent.Utils where
 
 import Control.Lens
+import Data.List
+import Data.Maybe
+import Data.Ord
 
 import Types
 
@@ -14,3 +18,12 @@ import Types
 aboveCounter :: AgentState -> Counter -> [AgentMessage]
 aboveCounter as c = map snd $ filter ((c<=).fst) $ as ^. messageSpace
 
+-- |Returns the first message (the one with the lowest counter)
+--  that has the correct constructor.
+firstWhere :: Prism' AgentMessage a -> [(Counter, AgentMessage)] -> Maybe a
+firstWhere l = head'
+               . mapMaybe (\(_,m) -> extractOver l id m)
+               . sortBy (comparing fst)
+   where
+      head' [] = Nothing
+      head' (x:_) = Just x
