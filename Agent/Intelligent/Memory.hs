ae11f2c234f8946e70557d483504f657e1b26217
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2015-05-15 03:47:17 +0200

    Re-did helper functions in Memory.hs.
    constructCell, constructEdge now return update functions instead of CellData/EdgeData values with undefined values. This has the advantage of enabling partial updates to stored memory, esp. in the case of AMVisualFree, which indicates the absence of information. Before, we'd have had no way of differentiating between the positive knowledge that a cell's entity should be set to Nothing and the negative knowledge that it should not be changed.

--- Agent/Intelligent/Memory.hs
+++ Agent/Intelligent/Memory.hs
@@ -5,7 +5,10 @@
 
 module Agent.Intelligent.Memory where
 
+import Control.Arrow
 import Control.Lens
+import Data.Functor.Monadic
+import Data.List
 import qualified Data.Map as M
 import Data.Maybe
 import Math.Geometry.Grid.Square (UnboundedSquareGrid(..))
@@ -32,6 +35,82 @@ constructWorldFromMemory as = World (WD time temperature)
       time = fromMaybe 0 $ firstWhere _AMTime msg
       temperature = fromMaybe Freezing $ firstWhere _AMTemperature msg
 
+-- |Reads out relevant messages from a message space and writes information
+--  about the world into the agent state.
+updateMemory :: AgentState -- ^Agent state. Its memory will only be written, no read.
+             -> [(Counter, AgentMessage)]
+             -> AgentState
+updateMemory xs as = as & memory %~ (M.union newCells *** M.union newEdges)
+   where
+      myPos = myPosition (as ^. messageSpace)
+
+      -- |Extracts the CellInd of a cell-related message (or the current possition
+      --  for local messages).
+      msgPos m = fromMaybe myPos (m ^? _1)
+
+      -- put the edge- and cell-related messages into bags indexed by
+      -- cell/edge index.
+      (cellMsg, edgeMsg) = foldl' preSort (M.empty, M.empty) xs
+
+      preSort (cs,es) (_,m) =
+         (fromMaybe cs (\m' -> M.insert (msgPos m) m' cs) (cellMessage m),
+          fromMaybe cs (\m' -> M.insert (m' ^. _1) m' es) (edgeMessage m))
+
+      newCells = M.mapWithKey
+
+-- |Gets the agent's latest position.
+myPosition :: [(Counter, AgentMessage)] -> CellInd
+myPosition = lastWhere AMPosition ^. _Just
+
+-- |Constructs a cell update function from agent messages.
+constructCell :: [AgentMessage]
+              -> (CellData -> CellData)
+constructCell i ms = foldl' addCellInfo cellEntity
+   where
+      -- First, set the agent to the appropriate type.
+      cellEntity = fromMaybe id (entity .~) $ constructAgent ms
+
+      addCellInfo f (AMVisualEntityName _ n) = (entity . _Just . name .~ n) . f
+      addCellInfo f (AMVisualEntityHealth _ n) = (entity . _Just . health .~ n) . f
+      addCellInfo f (AMVisualEntityStamina _ n) = (entity . _Just . stamina .~ n) . f
+
+      addCellInfo f (AMVisualFree _) = (entity .~ Nothing) . f
+      addCellInfo f (AMVisualPit _) = (pit .~ True) . f
+      addCellInfo f (AMVisualGold _ n) = (gold .~ n) . f
+      addCellInfo f (AMVisualMeat _ n) = (meat .~ n) . f
+      addCellInfo f (AMVisualFruit _ n) = (fruit .~ n) . f
+      addCellInfo f (AMVisualPlant _ n) = (plant .~ n) . f
+
+      addCellInfo f (AMLocalStench n) = (stench .~ n) . f
+      addCellInfo f (AMLocalBreeze n) = (breeze .~ n) . f
+      addCellInfo f (AMMyHealth n) = (entity . _Just . health .~ n) . f
+      addCellInfo f (AMMyStamina n) = (stench . _Just . stamina .~ n) . f
+      addCellInfo f (AMLocalGold n) = (gold .~ n) . f
+      addCellInfo f (AMLocalMeat n) = (meat .~ n) . f
+      addCellInfo f (AMLocalFruit n) = (fruit .~ n) . f
+
+-- |Constructs an partial entity from agent messages.
+--  If there's a Wumpus/Agent-message in the given list, the cell's entity
+--  will be set appropriately. Note, however, that all of its fields will be
+--  left undefined.
+constructEntity :: [AgentMessage]
+                -> (CellData -> CellData)
+constructEntity ms = (entity .~ agentKind)
+   where
+      agentKind = case (firstWhere _AMVisualAgent ms,
+                        firstWhere _AMVisualWumpus ms) of
+                     (Just _,_) -> Just VisualAgent{}
+                     (_, Just _) -> Just VisualWumpus{}
+                     (_,_) -> Nothing
+
+-- |Constructs an edge update from agent messages.
+constructEdge :: [AgentMessage] -> (EdgeData -> EdgeData)
+constructEdge = foldl' addEdgeInfo id
+   where
+      addEdgeInfo f (AMVisualEdgeDanger _ r) = (danger .~ r) . f
+      addEdgeInfo f (AMVisualEdgeFatigue _ r) = (fatigue .~ r) . f
+      addEdgeInfo f _ = f
+
 
 -- |Constructs a regular entity from a visual entity.
 --  The constructed agent's inventory will be assumed to be empty.
@@ -46,15 +125,18 @@ constructWorldFromMemory as = World (WD time temperature)
 --  @NoOp@ and @False@. When constructing oneself, they should be some chosen
 --  action (something that the decision maker wants simulated) and @True@ (
 --  so that the new perceptions can be read out again).
-constructAgent :: Action -- ^The action which the agent should perform, if asked.
+reconstructAgent :: Action -- ^The action which the agent should perform, if asked.
                -> Bool -- ^Whether the agent should store incoming messages.
+               -> World -- ^The current world. Constructed Wumpuses will store this.
                -> Entity VisualAgent VisualWumpus
                -> Entity (Agent SomeMind) (Wumpus SomeMind)
-constructAgent action s (Ag a) =
+reconstructAgent action s _ (Ag a) =
    Ag $ Agent (a ^. name)
               (a ^. direction)
               (a ^. health)
               (a ^. stamina)
               M.empty
               (SM $ DummyMind action s [])
---constructAgent _ _ (Wu w) = Wu $ Wumpus (WumpusMind )
+--reconstructAgent _ _ world (Wu w) =
+--   Wu $ Wumpus (WumpusMind )
+--               ()
