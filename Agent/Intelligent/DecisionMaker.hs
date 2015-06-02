6fb3ce72b44fc187f4d2b044bd67d1358bfa5360
Janos Tapolczai <janos.tapolczai@gmail.com>
2015-06-02 12:09:28 +0200

    Began writing DecisionMaker.hs.

---
+++ Agent/Intelligent/DecisionMaker.hs
@@ -0,0 +1,9 @@
+module Agent.Intelligent.DecisionMaker where
+
+import Control.Lens
+
+import Types
+
+-- |Makes a decision based on the affective evaluation of the world. If 
+makeDecision :: AgentComponent IO
+makeDecision = undefined
