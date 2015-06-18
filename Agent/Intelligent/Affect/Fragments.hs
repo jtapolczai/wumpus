58a18715b8f42f6123a60532445684ea9deb0255
Janos Tapolczai <janos.tapolczai@gmail.com>
2015-06-18 17:52:43 +0200

    Begun writing Affect.Fragments.

---
+++ Agent/Intelligent/Affect/Fragments.hs
@@ -0,0 +1,47 @@
+-- |Contains pre-made affective fragments from which one can piece together an
+--  agent's personality.
+module Agent.Intelligent.Affect.Fragments where
+
+-- |Returns the personality fragment belonging to an emotion and
+--  a type (currently supported: weak/strong).
+personalityFragment :: EmotionName -> String -> Filter AgentMessage
+personalityFragment Anger "weak" = weakAnger
+personalityFragment Anger "strong" = strongAnger
+
+personalityFragment Fear "weak" = weakFear
+personalityFragment Fear "strong" = strongFear
+
+personalityFragment Enthusiasm "weak" = weakEnthusiasm
+personalityFragment Enthusiasm "strong" = strongEnthusiasm
+
+personalityFragment Contentment "weak" = weakContentment
+personalityFragment Contentment "strong" = strongContentment
+
+personalityFragment _ x = error $ "personalityFragment called with unsupported type "++x
+
+weakAnger :: Filter AgentMessage
+weakAnger = todo "affectFragments"
+strongAnger :: Filter AgentMessage
+strongAnger = todo "affectFragments"
+
+weakFear :: Filter AgentMessage
+weakFear = todo "affectFragments"
+strongFear :: Filter AgentMessage
+strongFear = todo "affectFragments"
+
+weakEnthusiasm :: Filter AgentMessage
+weakEnthusiasm = todo "affectFragments"
+strongEnthusiasm :: Filter AgentMessage
+strongEnthusiasm = todo "affectFragments"
+
+weakContentment :: Filter AgentMessage
+weakContentment = todo "affectFragments"
+strongContentment :: Filter AgentMessage
+strongContentment = todo "affectFragments"
+
+hostileSocial :: Filter AgentMessage
+hostileSocial = todo "affectFragments"
+
+friendlySocial :: Filter AgentMessage
+friendlySocial = todo "affectFragments"
+
