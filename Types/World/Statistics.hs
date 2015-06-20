39061d3c3716b647c0d73946337c44bdfca5433b
Janos Tapolczai <janos.tapolczai@gmail.com>
2015-06-20 15:39:21 +0200

    WorldStats now counts the numbers of each type of items (Fruit, Gold, Meat) separately.

--- Types/World/Statistics.hs
+++ Types/World/Statistics.hs
@@ -3,6 +3,7 @@ module Types.World.Statistics where
 import qualified Data.Map as M
 
 import Types.Agent.Intelligent.Affect.Fragments
+import Types.World
 
 -- |A high-level description of an agent based on its personality fragments for
 --  anger, fear, enthusiasm, contentment, and sympathy.
@@ -17,8 +18,8 @@ data WorldStats = WS {
    _worldStatsNumAlive :: M.Map AgentIndex Int,
    _worldStatsNumWumpuses :: Int,
    _worldStatsNumHarvests :: Int,
-   _worldStatsNumItemsGiven :: Int,
-   _worldStatsNumGesturesSent :: Int,
+   _worldStatsNumItemsGiven :: M.Map Item Int,
+   _worldStatsNumGesturesSent :: Int,  --
    _worldStatsNumAttacksPerformed :: Int
    }
    deriving (Show, Eq, Ord)
--- World/Statistics.hs
+++ World/Statistics.hs
@@ -8,14 +8,14 @@ import qualified Data.Map as M
 import Types
 
 instance Monoid WorldStats where
-   mempty = WS (M.fromList $ map (,0) index) 0 0 0 0 0
+   mempty = WS (M.fromList $ map (,0) index) 0 0 (M.fromList $ map (,0) r) 0 0
       where
          r :: (Bounded t, Enum t) => [t]
          r = [minBound..maxBound]
 
          index = (,,,,) <$> r <*> r <*> r <*> r <*> r
    mappend (WS a w h i g p) (WS a' w' h' i' g' p') =
-      WS (M.unionWith (+) a a') (w+w') (h+h') (i+i') (g+g') (p+p')
+      WS (M.unionWith (+) a a') (w+w') (h+h') (M.unionWith (+) i i') (g+g') (p+p')
 
 
 agentDied :: AgentIndex -> WorldStats -> WorldStats
@@ -27,8 +27,8 @@ wumpusDied = numWumpuses -~ 1
 plantHarvested :: WorldStats -> WorldStats
 plantHarvested = numHarvests +~ 1
 
-itemGiven :: WorldStats -> WorldStats
-itemGiven = numItemsGiven +~ 1
+itemGiven :: Item -> WorldStats -> WorldStats
+itemGiven item = numItemsGiven . ix item +~ 1
 
 gestureSent :: WorldStats -> WorldStats
 gestureSent = numGesturesSent +~ 1
