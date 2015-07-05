68c597086a6408bc1895c586d8f3d6286f723f8f
Janos Tapolczai <janos.tapolczai@gmail.com>
2015-07-05 07:24:28 +0200

    Components can now communicate with each other via messages in callComponents.

--- Agent/Intelligent/MessageHandling.hs
+++ Agent/Intelligent/MessageHandling.hs
@@ -10,10 +10,10 @@ import Control.Monad
 import Types
 
 -- |Calls a list of components in succession. Each component receives the same
---  messages. Components may modify the agent state, but they cannot
---  communicate with each other via messages: any message they put into the
---  message space is ignored; any message they put into the 'newMessages' field
---  is collected but not passed to the next component.
+--  messages. Components may modify the agent state and communicate with each other
+--  by putting messages into 'newMessages'. Each component receives in its message space
+--  the messages of the initial agent state plus the sum of the new messages that previous
+--  components outputted. Messages written into the message space are ignored.
 --
 --  In the end, the modified agent state is returned, with its message space
 --  containing the list of all new messages that the components added. That is,
@@ -30,7 +30,7 @@ callComponents comps initAs = putMsg <$> foldM f (initAs, mempty) comps
       -- run the component with the initial messages.
       -- collect the newly added messages separately.
       f (curAs, ms) g = do
-         newAs <- g $ curAs & messageSpace .~ initAs ^. messageSpace
+         newAs <- g $ curAs & messageSpace .~ (view messageSpace initAs ++ ms)
          return (newAs & newMessages .~ mempty,
                  newAs ^. newMessages ++ ms)
 
