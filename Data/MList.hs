a1a3d826d954d94cb1500ec34c2c7ca905feb37f
Janos Tapolczai <janos.tapolczai@gmail.com>
2015-07-29 13:24:56 +0200

    Added Functor instance, takeM, fromMList to Data.MList.

--- Data/MList.hs
+++ Data/MList.hs
@@ -1,3 +1,16 @@
 module Data.MList where
 
 data MList m a = Nil | a :.: m (MList m a)
+
+instance Functor m => Functor (MList m) where
+   fmap _ Nil = Nil
+   fmap f (x :.: xs) = (f x) :.: (fmap (fmap f)) xs
+
+takeM :: Functor m => Int -> MList m a -> MList m a
+takeM 0 _ = Nil
+takeM _ Nil = Nil
+takeM n (x :.: xs) = x :.: fmap (takeM (n-1)) xs
+
+fromMList :: Monad m => MList m a -> m [a]
+fromMList Nil = pure []
+fromMList (x :.: xs) = (x:) <$> (xs >>= fromMList)
