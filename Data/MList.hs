89f2ebb213300844cfd5bf1771a0ecbaf4e68125
Janos Tapolczai <janos.tapolczai@gmail.com>
2015-08-01 12:08:11 +0200

    Changed MList.takeM.

--- Data/MList.hs
+++ Data/MList.hs
@@ -6,11 +6,15 @@ instance Functor m => Functor (MList m) where
    fmap _ Nil = Nil
    fmap f (x :.: xs) = (f x) :.: (fmap (fmap f)) xs
 
-takeM :: Functor m => Int -> MList m a -> MList m a
+takeM :: Applicative m => Int -> MList m a -> MList m a
 takeM 0 _ = Nil
 takeM _ Nil = Nil
+takeM 1 (x :.: _) = x :.: pure Nil
 takeM n (x :.: xs) = x :.: fmap (takeM (n-1)) xs
 
 fromMList :: Monad m => MList m a -> m [a]
 fromMList Nil = pure []
 fromMList (x :.: xs) = (x:) <$> (xs >>= fromMList)
+
+repeatM :: Functor m => m a -> m (MList m a)
+repeatM x = (:.: repeatM x) <$> x
