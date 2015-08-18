232b3244e9ee853f7982bd65e80a8ea58b9d7139
Janos Tapolczai <janos.tapolczai@gmail.com>
2015-08-18 23:11:02 +0200

    PersistentMessagesComponent now decreases the TTL every time.

--- Agent/Intelligent/PersistentMessages.hs
+++ Agent/Intelligent/PersistentMessages.hs
@@ -7,9 +7,11 @@ import Types
 
 import Debug.Trace
 
--- |Re-inserts messages that have a TTL greater than 0.
+-- |Re-inserts messages that have a TTL greater than 0 and
+--  decreases their TTL by 1.
 persistentMessagesComponent :: Monad m => AgentComponent m
 persistentMessagesComponent as = trace "[persistentMessagesComponent]" $ trace (replicate 80 '+') $ return $
-   addMessages (filter living $ view messageSpace as) as
+   addMessages (map decr $ filter living $ view messageSpace as) as
    where
       living (_,_,n) = n > 0
+      decr = _3 -~ 1
