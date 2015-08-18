fe4e3b94758a3e1324ff9a25322eac5de744457c
Janos Tapolczai <janos.tapolczai@gmail.com>
2015-08-18 18:33:39 +0200

    Closes #33.

--- Agent/Intelligent/PersistentMessages.hs
+++ Agent/Intelligent/PersistentMessages.hs
@@ -3,27 +3,13 @@ module Agent.Intelligent.PersistentMessages where
 import Control.Lens
 
 import Agent.Intelligent.MessageHandling
-import Agent.Intelligent.Utils
 import Types
 
 import Debug.Trace
 
--- |Re-inserts messages that should always stay in the agent's message space.
---
---  * World data
---  * Current position
---  * All non-imaginary messages
+-- |Re-inserts messages that have a TTL greater than 0.
 persistentMessagesComponent :: Monad m => AgentComponent m
 persistentMessagesComponent as = trace "[persistentMessagesComponent]" $ trace (replicate 80 '+') $ return $
-   addMessages (filter prisms $ view messageSpace as) as
+   addMessages (filter living $ view messageSpace as) as
    where
-      prisms (False,_,_) = True
-      prisms (_, x,_) = any ($ x)
-         [isP _AMPosition,
-          isP _AMTime,
-          isP _AMTemperature,
-          isP _AMPlanLocalBudget,
-          isP _AMPlanGlobalBudget,
-          isP _AMPlannedAction,
-          isP _AMPlanEmotion,
-          isP _AMPlanEmotionChanged]
+      living (_,_,n) = n > 0
