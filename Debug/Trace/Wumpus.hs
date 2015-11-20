ecd8d2609ab864c0c5c3bad8d926399c72e8ecd4
Janos Tapolczai <janos.tapolczai@gmail.com>
2015-11-20 23:26:41 +0100

    Added Wumpus-specific debug modules.

---
+++ Debug/Trace/Wumpus.hs
@@ -0,0 +1,14 @@
+module Debug.Trace.Wumpus (
+   Debug.Trace,
+   traceList,
+   trace',
+   ) where
+
+import Debug.Trace
+
+traceList :: Show a => [a] -> b -> b
+traceList [] y = y
+traceList (x:xs) y = traceShow x $ traceList xs y
+
+trace' :: Show a => a -> a
+trace' x = traceShow x x
