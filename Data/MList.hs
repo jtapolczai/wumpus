36d0870e56579b2c1c480be7e29548d3650b792b
Janos Tapolczai <janos.tapolczai@gmail.com>
2016-01-21 17:40:06 +0100

    Removed guff from MList.

--- Data/MList.hs
+++ Data/MList.hs
@@ -25,21 +25,3 @@ fmapM f (MList as) = MList $ do
                Just (x,xs) -> do
                   x' <- f x
                   return $ Just (x', fmapM f xs)
-
-{-
-takeM :: Applicative m => Int -> MList m a -> MList m a
-takeM 0 _ = Nil
-takeM _ Nil = Nil
-takeM 1 (x :.: _) = x :.: pure Nil
-takeM n (x :.: xs) = x :.: fmap (takeM (n-1)) xs
-
-fromMList :: Monad m => MList m a -> m [a]
-fromMList Nil = pure []
-fromMList (x :.: xs) = (x:) <$> (xs >>= fromMList)
-
-repeatM :: Functor m => m a -> m (MList m a)
-repeatM x = (:.: repeatM x) <$> x
-
-
-
--}
