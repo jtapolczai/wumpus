2163c5dc11d4b2568afed56876f23ff8752dca1f
Janos Tapolczai <janos.tapolczai@gmail.com>
2016-03-29 22:54:52 +0200

    Added world creation logic to World.Read.
    Added Data.Map.Utils.removeKeys.

--- Data/Map/Utils.hs
+++ Data/Map/Utils.hs
@@ -1,6 +1,7 @@
 -- |Utility functions for maps.
 module Data.Map.Utils where
 
+import qualified Data.Foldable as F
 import qualified Data.Map as M
 
 -- |Does a left join of two maps. The set of keys of the result is
@@ -32,3 +33,7 @@ fjoin :: Ord k
       -> M.Map k a
 fjoin x m n = M.mergeWithKey (\_ f x -> Just (f x)) (const M.empty) id m
               $ M.union n (fmap (const x) m)
+
+-- |Removes all given keys from the map. Safe.
+removeKeys :: Ord k => [k] -> M.Map k v -> M.Map k v
+removeKeys ks m = F.foldl' (flip M.delete) m ks 
--- World/Read.hs
+++ World/Read.hs
@@ -10,6 +10,7 @@ module World.Read (
    generateAgentLine,
    generateAgents,
    generateAgentsFile,
+   shuffle,
    ) where
 
 import Prelude hiding (log)
@@ -21,15 +22,19 @@ import Control.Monad
 import qualified Data.ByteString as BS
 import Data.Char (isSpace)
 import Data.Default
-import Data.List (intercalate)
+import qualified Data.Foldable as F
+import Data.Functor.Monadic
+import Data.List (intercalate, sortBy)
 import Data.List.Split (splitOn)
 import qualified Data.Map as M
+import Data.Map.Utils (removeKeys)
 import Data.Maybe (mapMaybe)
-import Data.Functor.Monadic
+import Data.Ord (comparing)
 import qualified Data.Tree as T
 import Data.Word
 import Math.Geometry.Grid.Square
 import Math.Geometry.Grid.SquareInternal (SquareDirection(..))
+import qualified System.Random as R
 
 import Agent.Intelligent.Affect.Fragments
 import Agent.Intelligent()
@@ -149,6 +154,7 @@ readWorld dir = do
       addEntity :: M.Map Word8 (Agent SomeMind, a) -> Int -> CellInd -> Pixel -> CellData -> CellData
       addEntity _ n i (255,0,0) c = c & entity ?~ Wu (Wumpus (SM $ WumpusMind (error "tried to access undefined wumpusMind!") i) ("w" ++ show n) cDEFAULT_WUMPUS_HEALTH cMAX_AGENT_STAMINA)
       addEntity _ _ _ (0,255,0) c = c & plant .~ Just cPLANT_MAX
+      addEntity _ _ _ (100,100,0) c = c & pit .~ True
       addEntity a _ _ (0,0,v) c | v > 0 = c & entity ?~ Ag (fst (a M.! v))
       addEntity _ _ _ _ c = c
 
@@ -198,6 +204,11 @@ trim :: String -> String
 trim = takeWhile (not.isSpace) . dropWhile isSpace
 
 
+
+
+-- Creating worlds
+--------------------------------------------------------------------------------
+
 -- |Creates a default Agent body from an agent
 defaultAgent :: AgentState -> Agent SomeMind
 defaultAgent as = Agent (as ^. name) North 1 1 (M.fromList [(Fruit,0), (Meat,0), (Gold,0)]) (SM as)
@@ -223,6 +234,22 @@ readBitmap = readBMP >=> (return.toArr.fromRight)
             pixels i n xs = (toCoord i, (xs ! 0, xs ! 1, xs ! 2))
                             : pixels (i+1) (n-1) (BS.drop 4 xs)
 
+-- |Creates a BMP given pixels and a default color.
+makeBitmap :: M.Map (Int, Int) Pixel -> Pixel -> (Int, Int) -> BMP
+makeBitmap foreground defColor (w,h) = packRGBA32ToBMP24 w h stream
+   where
+      background = M.fromList [((x,y), defColor) | y <- [0..h], x <- [0..w]]
+      pixels = M.union foreground background
+
+      pixelToWord8 :: Pixel -> [Word8]
+      pixelToWord8 (r,g,b) = [r,g,b,0]
+
+      stream :: BS.ByteString
+      stream = BS.concat
+               $ map (BS.pack . pixelToWord8 . snd)
+               $ sortBy (comparing fst)
+               $ M.toList pixels
+
 -- |Randomly generates a line that can be used in an agents-file in CSV-format.
 generateAgentLine :: Word8 -> IO String
 generateAgentLine w = do
@@ -239,3 +266,131 @@ generateAgents n = mapM generateAgentLine [1..n]
 -- |Generates an agent-file with the given name and the given number of agents.
 generateAgentsFile :: Word8 -> String -> IO ()
 generateAgentsFile n fn = generateAgents n >>= writeFile fn . unlines
+
+-- |The combination of all possible agent personalities. 32 items.
+--  The strings are combinations of the 5 personality fragments and the gestures
+--  "love" and "hate".
+allPersonalities :: [String]
+allPersonalities = map (intercalate ";") pers
+   where
+      psbcFrag = ["w","s"]
+
+      pers = [[a,f,e,c,h,"love","hate"] | a <- psbcFrag,
+                                          f <- psbcFrag,
+                                          e <- psbcFrag,
+                                          c <- psbcFrag,
+                                          h <- ["f", "h"]]
+
+
+-- |Creates populations where each personality occurs N times.
+--  The agent strings will be shuffled, consecutively numbered starting with 1,
+--  and in the format of an agent file.
+makePopulations :: Int -> IO [String]
+makePopulations subPopSize = map mkString . zip agentNames <$> shuffle rawList
+   where
+      agentNames = [1..(length rawList)]
+      rawList = concatMap (replicate subPopSize) allPersonalities
+      mkString (n,pers) = show n ++ ";" ++ pers
+
+-- |Randomly and uniformly selects elements from a collection
+--  and applies a function to them. The update function will be applied N times,
+--  where N is the size of the argument list. Each argument will only be used once.
+--  
+--  The function will be applied at most once to each element.
+distributeElems
+   :: Ord k
+   => [a] -- ^List of arguments to the application function.
+   -> (a -> v -> v) -- ^Function to apply to the selected element,
+                    --  if it has been selected.
+   -> M.Map k v -- ^Collection to which to apply the function.
+   -> IO (M.Map k v, [k]) -- ^The modified map and the keys to
+                            --  which the function was applied.
+distributeElems args appF coll = do
+   selectedKeys <- fst <$> takeRandomN (length args) (M.keys coll)
+   let (_,coll') = F.foldl' f (args, coll) selectedKeys
+       f (a:as, c) k = (as, M.adjust (appF a) k c)
+       f ([], _) _ = error "distributeElems: bug!"
+   return (coll', selectedKeys)
+
+-- |Applies a function with a certain probability onto each element of a collection.
+placeElems
+   :: Ord k
+   => (v -> v)
+   -> Float
+   -> M.Map k v
+   -> IO (M.Map k v)
+placeElems appF prob = mapM f
+   where
+      f x = do
+         (r :: Float) <- R.randomRIO (0,1)
+         return $ if r < prob then appF x else x   
+
+-- |Shuffles an array. For every index k and element v in the array A
+--  @P(v is put onto i) = 1 / length A@.
+--
+--  Not very efficient. Don't use this much.
+shuffle :: [a] -> IO [a]
+shuffle vec = view _1 <$> F.foldlM f ([], vec, length vec - 1) emptyVec
+   where 
+      emptyVec = replicate (length vec) undefined
+
+      deleteAt :: Int -> [a] -> [a]
+      deleteAt i xs = take i xs ++ drop (i+1) xs 
+
+      f (ret, src, maxInd) _ = do
+         i <- R.randomRIO (0,maxInd)
+         return ((src !! i) : ret, deleteAt i src, maxInd - 1)
+
+-- |Randomly selects N elements from a list. The first part is the list of
+--  included elements, the second part is the list of excluded ones.
+--
+--  Not very efficient. Don't use this much.
+takeRandomN :: Int -> [a] -> IO ([a], [a])
+takeRandomN n xs = (\ys -> (take n ys, drop n ys)) <$> shuffle xs
+
+-- |Takes a list of cells and populates the corresponding empty world with
+--  agents, Wumpuses, plants, pits, and gold.
+--
+--  Note: don't write the resultant maps directly as bitmaps if the given
+--  topology isn't rectangular. The output maps will have the same keys as the
+--  input topology.
+populateRandomWorld
+   :: M.Map (Int, Int) a
+   -> [Int] -- ^Agent names (numeric).
+   -> Int -- ^Number of Wumpuses.
+   -> Int -- ^Number of pits.
+   -> Float -- ^Probability of placing gold on a cell; 0 to 1.
+   -> Float -- ^Probability of placing a plant on a cell; 0 to 1.
+   -> IO (M.Map (Int, Int) Pixel, M.Map (Int, Int) Pixel) -- ^The entity-map and item-map.
+populateRandomWorld topology agents numWumpuses numPits goldProb plantProb = do
+   let t1 = fmap (const (0,0,0)) topology
+   (withAgents,   t2) <- remFrom t1 <$> distributeElems agents addAgent    t1
+   (withWumpuses, t3) <- remFrom t2 <$> distributeElems wumpuses addWumpus t2
+   (withPits,     t4) <- remFrom t3 <$> distributeElems pits addPit        t3
+   (withPlants,   t5) <- remFrom t4 <$> distributeElems plants addPlant    t4
+   withGold           <-                placeElems addGold goldProb        t5
+   let entityMap = t1
+                   `merge` withAgents
+                   `merge` withWumpuses
+                   `merge` withPits
+                   `merge` withPlants
+       itemMap = t1 `merge` withGold
+   return (entityMap, itemMap)
+   where
+      wumpuses = replicate numWumpuses undefined
+      pits = replicate numPits undefined
+      plants = replicate (floor $ fromIntegral (M.size topology) * plantProb) undefined
+
+      addAgent x _ = (0,0,fromIntegral x)
+      addWumpus _ _ = (255,0,0)
+      addPlant _ _ = (0,255,0)
+      addPit _ _ = (100,100,0)
+      addGold _ = (0,0,1)
+
+      remFrom t (x,keys) = (x, removeKeys keys t)
+
+      higherValue (0,0,0) r = r
+      higherValue l _ = l
+
+      infixl 5 `merge`
+      merge = M.unionWith higherValue
