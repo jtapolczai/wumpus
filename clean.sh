963e4f32626b8cc19c381883683e93df9d0e3fab
Janos Tapolczai <janos.tapolczai@gmail.com>
2016-04-05 15:47:27 +0200

    clean.sh now deleted "*.p_o" files too.

--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-find . \! -path "*/.cabal-sandbox/*" \! -path "*/dist/*" \( -name "*.dyn_hi" -delete -o -name "*.hi" -delete -o -name "*.dyn_o" -delete -o -name "*.o" -delete -o -name "*hi" -delete \)
+find . \! -path "*/.cabal-sandbox/*" \! -path "*/dist/*" \( -name "*.dyn_hi" -delete -o -name "*.hi" -delete -o -name "*.dyn_o" -delete -o -name "*.p_o" -delete -o -name "*.o" -delete -o -name "*hi" -delete \)
